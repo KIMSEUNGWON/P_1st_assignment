7683dc7e204c5d49e68ce24254bd1792316470db
KIMSEUNGWON <31675723+KIMSEUNGWON@users.noreply.github.com>
2017-09-08 02:12:24 +0900

    P_1st_Assignment p_prcatice2
    p_practice2 1st assignment

---
+++ P_1st_Assignment.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	printf("1.2017301014 is my student id number.\n");
+	printf("2. KIM SEUNG WON is my name.\n");
+	printf("3. 010-8460-7240 is my mobile phone number.\n");
+
+	printf("4.hesjs5@naver.com is my GitHub address.\n");
+
+	return 0;
+
+}
